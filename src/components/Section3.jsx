212def99d24b3ffb544e94b6a294e398b2f53e3e
Aniket25-11 <Www.aniketbhojapure@gmail.com>
2025-06-30 02:06:06 +0530

    Done some changes in Section3

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -1,38 +1,49 @@
-import React from 'react'
-import KiniBG from '../assets/Meet-kini-bg.png';
+import React from 'react';
+import KiniBG from '../assets/Kini-meet-bg.jpg';
+import Kinibg2 from '../assets/Kini-meet.webp';
 
 function Section3() {
     return (
         <>
-        {/* Meet Kini Section  */}
-            <div className='min-h-screen w-full flex justify-center items-center bg-pink-500 p-8 '>
-                <div className=' bg-center bg-cover text-center flex flex-col justify-between p-6 rounded-3xl min-h-96 max-w-xl w-full shadow-xl '
-                style={{ backgroundImage: `url(${KiniBG})`}}>
-                    <div>
-                        <p>Meet Kini, the first wearable using <br />
-                        non-invasive light technology <br />
-                        to support and improve female longevity
-                        </p>
-                    </div>
-                    <div >
-                        <div className='bg-gray-600 flex justify-between items-center p-4 text-white rounded-full w-full text-center'>
+            {/* Meet Kini Section */}
+            <div className="w-full h-screen pl-28 pr-28 pt-16 pb-16 mt-20">
+                {/* Main Section with Background Image */}
+                <div
+                    className="MeetKini w-full h-full rounded-3xl bg-green-400 pl-14 bg-cover bg-center flex flex-col justify-center items-center text-center"
+                    style={{ backgroundImage: `url(${KiniBG})` }}
+                >
+                    <h1 className="text-4xl font-bold text-white">
+                        Meet Kini,{" "}
+                        <span className="text-4xl font-bold text-gray-400">
+                            the first wearable using
+                            <br /> non-invasive light technology <br /> to support and
+                            improve female longevity
+                        </span>
+                    </h1>
+
+                    <div
+                        className="w-full bg-cover bg-center text-center pl-40 pr-60 py-16"
+                        style={{ backgroundImage: `url(${Kinibg2})` }}
+                    >
+                        <div className="bg-gray-700 text-white rounded-full px-9 py-4 font-semibold flex justify-between">
                             <div>
-                                <p>Get the new Kini now</p>
+                                <h1 className="px-7 py-2 text-2xl">Get to know Kini now!</h1>
                             </div>
-                            <div className='flex gap-10'>
-                                <p>$149</p>
-                                <button className='bg-white text-black rounded-lg px-4 py-2'>Buy now</button>
+                            <div className="flex justify-around">
+                                <div className="px-7 py-2 text-2xl">
+                                    <p>$149,00</p>
+                                </div>
+                                <button className="bg-white text-black px-9 py-4 rounded-3xl">
+                                    Buy now
+                                </button>
                             </div>
                         </div>
                     </div>
-                <div>
-
                 </div>
-                </div>
-
             </div>
         </>
-    )
+    );
 }
 
-export default Section3
+export default Section3;
+    
--- src/components/Section6.jsx
+++ src/components/Section6.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import sec6bg from '../assets/Section6-bg.webp';
 
 const Section6 = () => {
   return (
     <>
-    <div className='w-full h-screen bg-green-300 pl-28 pr-28 pt-16 pb-16'>
-        <div className='w-full h-full  rounded-3xl bg-red-400 pl-14 pt-28'>
+    <div className='w-full h-screen pl-28 pr-28 pt-16 pb-16'>
+        <div className='w-full h-full  rounded-3xl bg-red-400 pl-14 pt-28 bg-cover' style={{ backgroundImage: `url(${sec6bg})` }}>
             <h1 className='text-4xl  font-bold text-white'>Learn how CellLight powers your <br /> health. Innovative, <span className='text-gray-400'>science-backed <br /> technology created to fuel cellular <br /> energy and longevity. </span></h1>
 
             <button className='bg-white rounded-full px-9 py-4 mt-20 font-semibold'>Explore <span><i class="ri-arrow-right-line"></i></span></button>
